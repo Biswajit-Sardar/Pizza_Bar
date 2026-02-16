3f3eb0138343a2f0e7a8e39b543b503649e10022
Biswajit Sardar <biswajitsardar04082005@gmail.com>
2026-02-16 22:18:43 +0530

    final

---
+++ .next/dev/static/chunks/app_layout_tsx_336e69f5._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/app_pizza_globals_f4508d52.css",
+  "static/chunks/75973_next_dist_efa7c6c6._.js",
+  "static/chunks/lib_605364ed._.js"
+],
+    source: "dynamic"
+});
